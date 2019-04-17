6fe4b355a64c6e8f9d5efd4f800132065fc1c3d2
Kilian Stallinger <kp.stallinger@gmail.com>
2019-04-17 09:38:20 +0200

    add custom service worker

---
+++ src/service-worker.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.suppressWarnings()
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
+// install new sw then reload page
+self.addEventListener('message', msg => {
+  if (msg.data.action === 'skipWaiting') {
+    self.skipWaiting()
+  }
+})
